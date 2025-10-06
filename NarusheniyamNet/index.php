9f14d00ee56ea68100ddfe950f4465a0964787c2
kabinet303 <kabinet303@localhost.localdomain>
2025-10-06 09:36:45 +0300

    изменилась картинка

---
+++ NarusheniyamNet/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>НарушениямНет</title>
+    <link rel='icon' href='images/logo.jpg'>
+    <link rel='stylesheet' href='css/style.css'>
+</head>
+<body>
+    <header> <img src='images/logo.jpg' alt='логотип'>
+        <h1>НарушениямНет</h1>
+    </header>
+
+    <nav>
+        <a href="/demo-exam/NarusheniyamNet">Главная</a>
+        <a href="/demo-exam/NarusheniyamNet/admin">Админ-панель</a>
+    </nav>
+
+    <main>    
+        <footer>
+            <h3>2025</h3>
+        <footer>
+    </main>
+
+    <script src="js/script.js"></script>
+</body>
+</html>
